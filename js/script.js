a8c2e8d4a3e1bea9a7ac73bb662a70405435875f
luigiydm <71119496+luigiydm@users.noreply.github.com>
2022-11-23 21:47:37 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// init Isotope
+var $grid = $('.Servicios-list').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  resetFilterBtns();
+  $(this).addClass('active-filter-btn');
+  $grid.isotope({ filter: filterValue });
+});
+
+var filterBtns = $('.filter-button-group').find('button');
+function resetFilterBtns(){
+  filterBtns.each(function(){
+    $(this).removeClass('active-filter-btn');
+  });
+}
+
+function animateValue(id, start, end, duration) {
+  // assumes integer values for start and end
+  
+  var obj = document.getElementById(id);
+  var range = end - start;
+  // no timer shorter than 50ms (not really visible any way)
+  var minTimer = 50;
+  // calc step time to show all interediate values
+  var stepTime = Math.abs(Math.floor(duration / range));
+  
+  // never go below minTimer
+  stepTime = Math.max(stepTime, minTimer);
+  
+  // get current time and calculate desired end time
+  var startTime = new Date().getTime();
+  var endTime = startTime + duration;
+  var timer;
+
+  function run() {
+      var now = new Date().getTime();
+      var remaining = Math.max((endTime - now) / duration, 0);
+      var value = Math.round(end - (remaining * range));
+      obj.innerHTML = value;
+      if (value == end) {
+          clearInterval(timer);
+      }
+  }
+  
+  timer = setInterval(run, stepTime);
+  run();
+}
+
+animateValue("value", 0, 21000, 8000);
